6a2dde82d848bc6d341b6b78b50a55ce1b515163
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-04-21 15:37:15 -0300

    Otimização do Pacman deixada para o fim do script

--- Manjaro/afterinstall.sh
+++ Manjaro/afterinstall.sh
@@ -19,11 +19,6 @@ echo "====================================================================="
 pacman -Syyuu --noconfirm
 
 echo "====================================================================="
-echo "==================== optimizing pacman database ====================="
-echo "====================================================================="
-pacman-optimize
-
-echo "====================================================================="
 echo "========= installing development tools and needed packages =========="
 echo "====================================================================="
 pacman -S --noconfirm android-tools apache apache-ant atom bower clang cmake \
@@ -51,18 +46,16 @@ npm install -g bower grunt
 npm install -g ionic cordova
 
 echo "====================================================================="
-echo "============== enabling and initializing PostgreSQL ================="
-echo "====================================================================="
-systemctl enable postgresql.service
-su - postgres -c "initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'"
-systemctl restart postgresql.service
-
-echo "====================================================================="
 echo "=============== removing some unnecessary packages =================="
 echo "====================================================================="
 pacman -Rns steam-manjaro
 pacman -Qdtq | pacman -Rs -
 pacman -Sc
+
+echo "====================================================================="
+echo "==================== optimizing pacman database ====================="
+echo "====================================================================="
+pacman-optimize
 
 echo "====================================================================="
 echo "================= fixing Java fonts antialiasing ===================="
@@ -74,7 +67,6 @@ _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true \
   -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \
   -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 JAVA_FONTS=/usr/share/fonts/TTF
-
 EOF
 
 cat <<EOF >> /etc/bash.bashrc
@@ -83,5 +75,11 @@ cat <<EOF >> /etc/bash.bashrc
 _SILENT_JAVA_OPTIONS="$_JAVA_OPTIONS"
 unset _JAVA_OPTIONS
 alias java='java "$_SILENT_JAVA_OPTIONS"'
-
 EOF
+
+echo "====================================================================="
+echo "============== enabling and initializing PostgreSQL ================="
+echo "====================================================================="
+systemctl enable postgresql.service
+su - postgres -c "initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'"
+systemctl restart postgresql.service
