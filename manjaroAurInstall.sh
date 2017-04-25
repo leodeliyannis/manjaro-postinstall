2a2505fcd9615b6358a46236fd96e6951b36ccfa
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-04-25 15:26:24 -0300

    Sublime Text 3 adicionado

--- manjaroAurInstall.sh
+++ manjaroAurInstall.sh
@@ -2,7 +2,7 @@ echo "====================================================================="
 echo "======= installing packages from AUR (Arch Users Repository) ========"
 echo "====================================================================="
 yaourt -S --noconfirm android-studio astah-community google-chrome \
-  pepper-flash ttf-mac-fonts ttf-monaco ttf-ms-fonts
+  pepper-flash sublime-text-dev ttf-mac-fonts ttf-monaco ttf-ms-fonts
 if [ $? != 0 ]; then
   echo ""
   echo "ERROR running yaourt -- must check if all packages are available"
