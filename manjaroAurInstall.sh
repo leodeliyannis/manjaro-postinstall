682a83bff6a407e1feac6138101a0029afffa8ae
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-05-08 08:19:42 -0300

    Xampp adicionado

--- manjaroAurInstall.sh
+++ manjaroAurInstall.sh
@@ -1,8 +1,8 @@
 echo "====================================================================="
 echo "======= installing packages from AUR (Arch Users Repository) ========"
 echo "====================================================================="
-yaourt -S --noconfirm android-studio astah-community google-chrome \
-  pepper-flash sublime-text-dev ttf-mac-fonts ttf-monaco ttf-ms-fonts
+yaourt -S --noconfirm android-studio astah-community google-chrome pepper-flash \
+  sublime-text-dev ttf-mac-fonts ttf-monaco ttf-ms-fonts xampp
 if [ $? != 0 ]; then
   echo ""
   echo "ERROR running yaourt -- must check if all packages are available"
