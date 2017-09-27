1aff0c6053a677cded3707ba052dffd03698500b
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-09-27 14:55:26 -0300

    Adicionado pencil aos programas do AUR

--- manjaroAurInstall.sh
+++ manjaroAurInstall.sh
@@ -3,7 +3,7 @@ echo "======= installing packages from AUR (Arch Users Repository) ========"
 echo "====================================================================="
 yaourt -S --noconfirm --needed \
   android-studio astah-community google-chrome mattercontrol \
-  sublime-text-dev ttf-monaco wps-office
+  pencil sublime-text-dev ttf-monaco wps-office
 if [ $? != 0 ]; then
   echo ""
   echo "ERROR running yaourt -- must check if all packages are available"
