20c7ce8814184408549b141e87b4158ceadb0edf
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-10-17 15:50:44 -0200

    Removido mattercontrol; adicionado freshplayerplugin

--- manjaroAurInstall.sh
+++ manjaroAurInstall.sh
@@ -2,8 +2,8 @@ echo "====================================================================="
 echo "======= installing packages from AUR (Arch Users Repository) ========"
 echo "====================================================================="
 yaourt -S --noconfirm --needed \
-  android-studio astah-community google-chrome mattercontrol \
-  pencil sublime-text-dev ttf-monaco wps-office
+  android-studio astah-community freshplayerplugin google-chrome pencil \
+  sublime-text-dev ttf-monaco wps-office
 if [ $? != 0 ]; then
   echo ""
   echo "ERROR running yaourt -- must check if all packages are available"
