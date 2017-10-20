6564328cf061f7b36040a00021e0d83ecc722987
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-10-20 14:47:09 -0200

    Adicionado xmind aos pacotes AUR p/ instalação

--- manjaroAurInstall.sh
+++ manjaroAurInstall.sh
@@ -3,7 +3,7 @@ echo "======= installing packages from AUR (Arch Users Repository) ========"
 echo "====================================================================="
 yaourt -S --noconfirm --needed \
   android-studio astah-community freshplayerplugin google-chrome pencil \
-  sublime-text-dev ttf-monaco wps-office
+  sublime-text-dev ttf-monaco wps-office xmind
 if [ $? != 0 ]; then
   echo ""
   echo "ERROR running yaourt -- must check if all packages are available"
